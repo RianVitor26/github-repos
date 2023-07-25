dc325a393e224ef1f2ffd92ab8c8faa6705e8e1e
RianVitor26 <rianvitorlhe@gmail.com>
2023-07-25 07:40:53 -0300

    style: ajustes na borda do aside

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,7 @@ import { Repositories } from "@/components/Repositories";
 export default function Home() {
   return (
     <div className="w-full bg-gray-800 flex flex-col md:flex-row md:justify-between">
-      <aside className="w-11/12 mx-auto flex flex-col items-center bg-gray-700 mb-10 rounded-md p-3 md:w-2/5  md:fixed md:top-0 md:left-0 md:h-screen">
+      <aside className="w-11/12 mx-auto flex flex-col items-center bg-gray-700 mb-10 rounded-md p-3 md:w-2/5  md:fixed md:top-0 md:left-0 md:h-screen md:border-r-2 md:border-gray-800">
           <Profile/>
       </aside>
       <span className="md:w-2/5 hidden md:block"></span>
