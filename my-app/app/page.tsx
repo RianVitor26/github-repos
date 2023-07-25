bbed6caf2def18d82911834b1e33dcd5aff141cc
RianVitor26 <rianvitorlhe@gmail.com>
2023-07-25 07:37:00 -0300

    feat: ajustes na responsividade desktop

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,11 +5,12 @@ import { Repositories } from "@/components/Repositories";
 
 export default function Home() {
   return (
-    <div className="w-full bg-gray-800 flex flex-col md:flex-row">
-      <aside className="w-11/12 mx-auto flex flex-col items-center bg-gray-700 mb-10 rounded-md p-3 md:w-1/2">
+    <div className="w-full bg-gray-800 flex flex-col md:flex-row md:justify-between">
+      <aside className="w-11/12 mx-auto flex flex-col items-center bg-gray-700 mb-10 rounded-md p-3 md:w-2/5  md:fixed md:top-0 md:left-0 md:h-screen">
           <Profile/>
       </aside>
-      <main className="w-11/12 min-h-screen mx-auto bg-gray-700 rounded-md p-3 md:w-1/2">
+      <span className="md:w-2/5 hidden md:block"></span>
+      <main className="w-11/12 min-h-screen mx-auto bg-gray-700 rounded-md p-3 md:w-3/5">
           <Repositories/>
       </main>
     </div>
