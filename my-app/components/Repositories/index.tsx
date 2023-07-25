8c3f0c9c0f4a5f2bca36f1b7d43bd5310237e699
RianVitor26 <rianvitorlhe@gmail.com>
2023-07-25 06:57:57 -0300

    desenvolvendo card

--- my-app/components/Repositories/index.tsx
+++ my-app/components/Repositories/index.tsx
@@ -25,11 +25,11 @@ interface LanguageColorMapping {
 const colorMapping: LanguageColorMapping = {
   JavaScript: 'bg-yellow-300',
   Python: 'bg-blue-300',
-  Java: 'bg-red-300',
+  Java: 'bg-orange-700',
   C: 'bg-gray-400',
   CSS: 'bg-blue-500',
   TypeScript: 'bg-sky-500',
-  HTML: 'bg-orange-800',
+  HTML: 'bg-orange-900',
 };
 
 
@@ -59,19 +59,30 @@ export const Repositories = () => {
       <div className="w-full grid grid-cols-1 place-items-center gap-3">
         {search.length > 0 ? (
           <>
-            {filteredRepos.map((repo) => {
+        {filteredRepos.map((repo) => {
+              const languageColor = colorMapping[repo.language] || 'bg-gray-300';
+
               return (
                 <Card
                   key={repo.name}
                   className="mt-6 w-full bg-gray-800 rounded-md p-5"
                 >
                   <CardBody>
-                    <h1 className="mb-2 text-gray-100">{repo.name}</h1>
+                    <h1 className="mb-2 text-gray-100 font-bold">{repo.name}</h1>
                     <h2 className="text-gray-300">{repo.description}</h2>
                   </CardBody>
-                  <CardFooter className="mt-2">
+                  <CardFooter className="mt-2 flex justify-between">
+                    <div className="flex flex-col gap-2">
+                      <div
+                        className="text-gray-400 flex items-center gap-2"
+                      >
+                        <span className={`w-4 h-4 rounded-full ${languageColor}`}></span>
+                        {repo.language ? repo.language : "None"}
+                      </div>
+                      <div className="text-gray-400 flex items-center gap-2"><FaCodeFork/>Forks {repo.forks}</div>
+                    </div>
                     <Link target="_blank" href={repo.clone_url}>
-                      <Button className="bg-orange-500 text-gray-800 p-2 rounded-md">
+                      <Button className="p-2 rounded-md bg-emerald-500">
                         Código fonte
                       </Button>
                     </Link>
@@ -100,7 +111,7 @@ export const Repositories = () => {
                         className="text-gray-400 flex items-center gap-2"
                       >
                         <span className={`w-4 h-4 rounded-full ${languageColor}`}></span>
-                        {repo.language ? repo.language : "Repo sem linguagem"}
+                        {repo.language ? repo.language : "None"}
                       </div>
                       <div className="text-gray-400 flex items-center gap-2"><FaCodeFork/>Forks {repo.forks}</div>
                     </div>
